d09ba03c78c3a8d02bb03b832f85d45348b7feba
Tykhon <richard.lee970112@gmail.com>
2022-12-05 12:06:39 +0900

    rooms edit

--- base/views.py
+++ base/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id':1, 'name': 'Lets learn python!'},
+    {'id':2, 'name': 'Design with me'},
+    {'id':3, 'name': 'Frontend developers'},
+]
 
 def home(request):
     return render(request, 'home.html')
